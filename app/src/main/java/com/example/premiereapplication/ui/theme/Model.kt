908157f2c8fd021695b1a07a6a46cac18a5aba06
Geoffreynji <geoffrey.naji@etud.univ-jfc.fr>
2024-11-13 14:48:36 +0100

    modele en place

--- app/src/main/java/com/example/premiereapplication/ui/theme/Model.kt
+++ app/src/main/java/com/example/premiereapplication/ui/theme/Model.kt
@@ -232,3 +232,22 @@ data class CrewSerie(
     val popularity: Double,
     val profile_path: String
 )
+
+// Partie exam
+data class examClass(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
+
+data class Result(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val id: Int,
+    val name: String,
+    val original_language: String,
+    val original_name: String,
+    val overview: String,
+    val poster_path: String
+)
